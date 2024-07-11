85075986f659147d4dabef297eb0d35e82a44de9
Alfazal007 <alfazal7777@gmail.com>
2024-07-11 15:11:56 +0530

    unfollow added

--- routes/followRoutes.go
+++ routes/followRoutes.go
@@ -10,5 +10,6 @@ import (
 func FollowRouter(apiCfg *controllers.ApiCfg) *chi.Mux {
 	r := chi.NewRouter()
 	r.Post("/follow", controllers.VerifyJWT(apiCfg, http.HandlerFunc(apiCfg.FollowUser)).ServeHTTP)
+	r.Post("/unfollow", controllers.VerifyJWT(apiCfg, http.HandlerFunc(apiCfg.UnfollowUser)).ServeHTTP)
 	return r
 }
